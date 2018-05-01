c9a1f34fd94b051441cc4cb7f9a57105e61fff99
Vladimir Melnichuk <tormund@zeoalliance.com>
2018-05-01 14:39:38 +0300

    replace deperacated code

--- nuuid.nim
+++ nuuid.nim
@@ -9,6 +9,7 @@ import strutils
 import times
 import random
 
+
 randomize()
 
 const pattern = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
@@ -22,4 +23,4 @@ proc generateUUID*(): string =
     var r = toInt(toFloat(d) + random(1.0) * 16) %% 16
     d = toInt(floor(toFloat(d) / 16))
     toHex(if c == 'x': r else: r and 0x3 or 0x8, 1)
-  toLower(join(pattern.mapIt(string, if it == 'x' or it == 'y': fn(it) else: $it)))
+  toLowerAscii(join(pattern.mapIt(string, if it == 'x' or it == 'y': fn(it) else: $it)))
