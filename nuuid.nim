02cb1cf4b253f0e6539f58816b43993137980128
Dominik Picheta <dominikpicheta@gmail.com>
2019-01-23 21:17:43 +0000

    Fixes for Nim HEAD. Refs https://github.com/nim-lang/Nim/issues/10433.

--- nuuid.nim
+++ nuuid.nim
@@ -10,8 +10,6 @@ import times
 import random
 
 
-randomize()
-
 const pattern = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
 
 proc getTime(): int  =
@@ -23,4 +21,4 @@ proc generateUUID*(): string =
     var r = toInt(toFloat(d) + random(1.0) * 16) %% 16
     d = toInt(floor(toFloat(d) / 16))
     toHex(if c == 'x': r else: r and 0x3 or 0x8, 1)
-  toLowerAscii(join(pattern.mapIt(string, if it == 'x' or it == 'y': fn(it) else: $it)))
+  toLowerAscii(join(pattern.mapIt(if it == 'x' or it == 'y': fn(it) else: $it)))
