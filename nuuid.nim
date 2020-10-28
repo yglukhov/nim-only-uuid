078f473fc811793119df55b766a3670a62f988c2
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2020-10-28 16:38:19 +0200

    Fixed deprecation error

--- nuuid.nim
+++ nuuid.nim
@@ -18,7 +18,7 @@ proc getTime(): int  =
 proc generateUUID*(): string =
   var d = getTime()
   proc fn(c : char): string =
-    var r = toInt(toFloat(d) + random(1.0) * 16) %% 16
+    var r = toInt(toFloat(d) + rand(1.0) * 16) %% 16
     d = toInt(floor(toFloat(d) / 16))
     toHex(if c == 'x': r else: r and 0x3 or 0x8, 1)
   toLowerAscii(join(pattern.mapIt(if it == 'x' or it == 'y': fn(it) else: $it)))
